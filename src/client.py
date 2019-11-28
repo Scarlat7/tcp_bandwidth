df44977cf37d3641b386362d3217e771a4331084
Scarlat7 <nataliarampon@hotmail.com>
2019-11-28 18:50:36 -0500

    Removes measure from client

--- src/client.py
+++ src/client.py
@@ -4,47 +4,40 @@ import socket
 import argparse
 import time as t
 
+def client(args):
+	MEGA = 2**20
+	TCP_IP = 'localhost'
+	TCP_PORT = args.port 
+	DEFAULT_TIME = 120
+	MSG_SIZE = 1024
+	TIME =  DEFAULT_TIME if (args.time == None) else args.time
 
-parser = argparse.ArgumentParser(description='Client for bandwidth test.')
-parser.add_argument('-p', '--port', type=int, help='server connection port', required=True)
-parser.add_argument('-t', '--time', type=int, help='how long the test will run (in seconds)')
+	# Creates TCP socket (SOCK_STREAM) with AF_INET address family (host, port)
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-args = parser.parse_args()
+	# Connects to the server
+	s.connect((TCP_IP, TCP_PORT))
 
-MEGA = 2**20
-TCP_IP = 'localhost'
-TCP_PORT = args.port 
-DEFAULT_TIME = 120
-TIME =  DEFAULT_TIME if (args.time == None) else args.time
-MESSAGE = "X"*1024
+	print("Connected to server {} on port {}".format(TCP_IP, TCP_PORT))
 
-# Creates TCP socket (SOCK_STREAM) with AF_INET address family (host, port)
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	start = t.time()
+	data_sent = 0
 
-# Connects to the server
-s.connect((TCP_IP, TCP_PORT))
+	while(t.time() - start < TIME):
 
-print("Connected to server {} on port {}".format(TCP_IP, TCP_PORT))
+		start_inst = t.time()
 
-start = t.time()
-data_sent = 0
+		# Sends 1024-byte message
+		s.send(b'X'*MSG_SIZE);
 
-while(t.time() - start < TIME):
-	
-	encoded_msg = MESSAGE.encode()
+	# Closes socket
+	s.close()
 
-	start_inst = t.time()
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description='Client for bandwidth test.')
+	parser.add_argument('-p', '--port', type=int, help='server connection port', required=True)
+	parser.add_argument('-t', '--time', type=int, help='how long the test will run (in seconds)')
 
-	# Sends 1024-byte message
-	s.send(encoded_msg)
+	args = parser.parse_args()
 
-	data_sent += len(encoded_msg)
-
-	inst_speed = len(encoded_msg)/((t.time()-start_inst)*MEGA)
-	print("{} MB/s".format(inst_speed))
-
-print("Total data sent over {} seconds: {} bytes".format(TIME, data_sent))
-print("For an average of {} MB/s".format(data_sent/(TIME*MEGA)))
-
-# Closes socket
-s.close()
+	client(args)
