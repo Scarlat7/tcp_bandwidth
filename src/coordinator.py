eb0892e2d3494f5795d62d0df95d5c69a00413ae
Scarlat7 <nataliarampon@hotmail.com>
2019-12-14 07:12:48 -0500

    feat(coordinator): separating server and client coordination

--- src/coordinator.py
+++ src/coordinator.py
@@ -6,23 +6,36 @@ from client import client
 from time import sleep
 
 def coordinator(args):
-	port = args.port
 	threads = []
 	seconds = 10
 
-	for i in range(args.clients,0,-1):
-		threads.append(Thread(target = server, args = [port+i-1]))
-		threads.append(Thread(target = client, args = [port+i-1, i*seconds]))
+	create_threads(threads, args, seconds)
+	launch_threads(threads, args, seconds)
 
+	for i in range(max(args.servers, args.clients))
+		threads[i].join()
+
+def create_threads(threads, args, seconds):
+	for i in range(args.clients,0,-1):
+		threads.append(Thread(target = client, args = [args.ip, args.port+i-1, i*seconds]))
+
+	for i in range(args.servers,0,-1):
+		threads.append(Thread(target = server, args = [args.port+i-1]))
+
+def launch_threads(threads, args, seconds):
 	for i in range(args.clients):
-		threads[2*i].start()
-		threads[2*i+1].start()
+		threads[i].start()
 		sleep(seconds)
+
+	for i in range(args.servers):
+		threads[i].start()
+
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description='Coordinator for bandwidth test.')
 	parser.add_argument('-p', '--port', type=int, help='server connection port', required=True)
-	parser.add_argument('-c', '--clients', type=int, help='number of parallel connections', required=True)
+	parser.add_argument('-c', '--clients', type=int, default=0, help='number of parallel clients', required=False)
+	parser.add_argument('-s', '--servers', type=int, default=0, help='number of parallel servers', required=False)
 
 	args = parser.parse_args()
 
